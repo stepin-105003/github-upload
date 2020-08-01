a928aa4f70c6cfcec6c82d2405e0044f4c659f2e
stepin-105003 <69052039+stepin-105003@users.noreply.github.com>
2020-08-01 21:33:50 +0530

    updated
    untitled1

--- Untitled1.c
+++ Untitled1.c
@@ -2,25 +2,15 @@
 #include<string.h>
 #include<math.h>
 #include<stdlib.h>
-main()
-{
-int i;
-char *word[3];
+main(){
+
+int i=10;
+char x='A';
+float b=2.5;
 void * gp;
-word[0]="one";
-word[1]="two";
-word[2]="three";
-for(i=0;i<3;i++)
-{
-    printf(" word is %s \n",word[i]);
-}
-
-gp= word;
-for(i=0;i<3;i++)
-{
-    printf("%s \n",(*(char **)gp));
-    gp= (char **)gp +1;
-}
+gp=&x;
+gp=&i;
+gp=&b;
+printf("%f",*(float *)gp);
 
 }
-
